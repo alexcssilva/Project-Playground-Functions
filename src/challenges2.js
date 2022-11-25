ea5a84506ca6c212ec1c8d974e1ba712840c7706
Alex Silva <alexcssilva@gmail.com>
2022-11-25 15:52:12 -0300

    feat: added README.md

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,21 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
+function techList(techNames, name) {
+  let techNames = [];
+  let objectNames = '';
+  let name = ''
 
+  for (techs of techNames) {
+    if (tech < techNames) {
+      objectNames = {
+        tech: "NomeTech",
+        name: name,
+      }
+      console.log()
+    }
+  }
+
+}
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],'Lucas'));
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
